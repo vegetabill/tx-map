2ac1e03de8b146d8624470c7b29b24b004331213
Bill D <355438+vegetabill@users.noreply.github.com>
2020-09-17 18:41:12 -0700

    migrate to using a TCP server so we can serve multiple concurrent clients

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-const { createInterface } = require("readline");
+const net = require("net");
+const readLines = require("./lib/line-reader");
 const { startSession } = require("./lib/session");
+const Context = require("./lib/context");
 
-const session = startSession();
+const root = new Context();
 
-const input = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false,
-});
+const server = net.createServer((client) => {
+  const outputListener = (msg) => {
+    client.write(msg + "\n");
+  };
 
-console.log("tx-map console");
+  const session = startSession(root, outputListener);
+  console.debug(`Client connected: session ${session.id}`);
 
-input.on("line", (line) => {
-  try {
+  client.write("Tx-Map Console - Commands end with newline. \\q to quit.\n\n");
+
+  readLines(client, (line) => {
     session.processLine(line);
-  } catch (e) {
-    console.error(e);
-    if (!e.recoverable) {
-      process.exit(-1);
-    }
-  }
+  });
+
+  client.on("close", () => session.close());
+  client.on("end", () => session.close());
+  client.on("error", (err) => console.error(err));
 });
 
-input.on("close", () => console.log("👋"));
+const port = process.env.PORT || 7777;
+server.listen(port, () => {
+  console.log(`Tx Map server listening on port ${port}`);
+});
+server.on("error", (err) => {
+  throw err;
+});
